0246ecc2828f71562f7de8ee9430066c84cc1d7c
wodie99 <wodie99@wodie.net>
2022-06-08 13:38:03 +0200

    #24 set colors to app and change font size for videoformat 375 x 667

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,27 +3,31 @@ import "../style/Header.css"
 import {useContext} from "react";
 import {AuthContext} from "../context/AuthProvider";
 
-export  default function Header() {
+export default function Header() {
     const {token} = useContext(AuthContext);
 
     return <header>
-        <div className={"row1"}>
-            <span className={"header-title"}><b>bicycle tour organizer</b></span>
-        </div>
-        {token ?
-            <div className={"row2"}>
-                <div>
-                    <NavLink to="/">Übersicht</NavLink>
-                </div>
-                <div>
-                    <NavLink to="/userpage">Useransicht</NavLink>
-                </div>
-                <div>
-                    <NavLink to="/logout">Logout</NavLink>
-                </div>
+        <div className={"header-area"}/>
+        <div className={"header-content"}>
+            <div className={"row1"}>
+
+                <span className={"header-title"}><b>bicycle tour organizer</b></span>
             </div>
-            :
-            <p className={"row2-login"}>Login</p>
-        }
+            {token ?
+                <div className={"row2"}>
+                    <div>
+                        <NavLink to="/">Übersicht</NavLink>
+                    </div>
+                    <div>
+                        <NavLink to="/userpage">Useransicht</NavLink>
+                    </div>
+                    <div>
+                        <NavLink to="/logout">Logout</NavLink>
+                    </div>
+                </div>
+                :
+                <p className={"row2-login"}>Login</p>
+            }
+        </div>
     </header>
 }
